58a19ff21166685e899e9f4dd17db89f30612b62
robin hunter <rbnrggs@yahoo.com>
2024-02-12 14:42:00 -1000

    update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
+import ReactDOM from 'react-dom'
 import { BrowserRouter as Router } from "react-router-dom"
 import App from './components/App'
 import './index.css'
 
-import { ThemeProvider } from "@material-tailwind/react";
+import { ThemeProvider } from '@material-tailwind/react'
  
 
 ReactDOM.createRoot(document.getElementById('root')).render(
