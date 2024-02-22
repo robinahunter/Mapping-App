a9454a9f7a58e4cf38d850e34f5b358f8019207c
robin hunter <rbnrggs@yahoo.com>
2024-02-21 17:16:26 -1000

    update

--- frontend/src/components/HomePage/index.jsx
+++ frontend/src/components/HomePage/index.jsx
@@ -1,164 +1,325 @@
+// import React, { useState, useEffect } from 'react';
+// import esriConfig from '@arcgis/core/config';
+// import Map from '@arcgis/core/Map';
+// // import axios from 'axios';
+// import VectorTileLayer from '@arcgis/core/layers/VectorTileLayer';
+// import Basemap from '@arcgis/core/Basemap';
+// import BasemapToggle from '@arcgis/core/widgets/BasemapToggle';
+// import SceneView from '@arcgis/core/views/SceneView';
+// import FeatureLayer from '@arcgis/core/layers/FeatureLayer';
+// // import Symbol from "@arcgis/core/symbols/Symbol.js";
+// // import Renderer from '@arcgis/core/renderers/Renderer.js';
+// import PopupTemplate from "@arcgis/core/PopupTemplate.js";
+// // import WebStyleSymbol from "@arcgis/core/symbols/WebStyleSymbol.js";
+// // import SimpleRenderer from "@arcgis/core/renderers/SimpleRenderer.js";
+// // import calciteIcons from "@esri/calcite-ui-icons/docs/icons.json";
+// // import { loadModules } from 'esri-loader';
+// import { TailSpin } from "react-loader-spinner";
+
+
+// const HomePage = () => {
+//     useEffect(() => {
+//       const initializeArcGIS = async () => {
+//         esriConfig.apiKey = process.env.REACT_APP_ESRI_KEY; // Set your API key from environment variables
+//         // esriConfig.apiKey = process.env.REACT_APP_MAP_FEATUREDLAYER_KEY; // Set your API key from environment variables
+  
+//         // Loading spinner
+//             const [loading, setLoading] = useState(false);
+          
+//             useEffect(() => {
+//               setLoading(true);
+//               setTimeout(() => {
+//                 setLoading(false);
+//               }, 3000);
+//             }, []);
+
+            
+//         // Create a VectorTileLayer from a style URL
+//         const mapBaseLayer = new VectorTileLayer({
+//           url: 'https://arcgis.com/sharing/rest/content/items/b5676525747f499687f12746441101ef/resources/styles/root.json',
+//         });
+  
+//         // Create a Basemap with the VectorTileLayer
+//         const customBasemap = new Basemap({
+//             baseLayers: [mapBaseLayer],
+//             title: 'Street',
+//             id: 'streets',
+//             thumbnailUrl:
+//             'https://arcgis.com/sharing/rest/content/items/b5676525747f499687f12746441101ef/info/thumbnail/ago_downloaded.png',
+//         });
+        
+//         const map = new Map({
+//             basemap: 'arcgis/topographic', //streets-navigation-vector, arcgis/topographic, customBasemap
+//             ground: 'world-elevation',
+//         });
+
+//         const initCamera = {
+//             heading: 1,
+//             tilt: 1,
+//             position: {
+//                 latitude: 20.753,
+//                 longitude: -156.449,
+//                 z: 200000,
+//             },
+//         };
+        
+//         const view = new SceneView({
+//             container: 'viewDiv',
+//             map: map,
+//             camera: initCamera,
+//         });
+        
+//         view.when(() => {
+//             // Add a BasemapToggle widget to toggle between basemaps
+//             const toggle = new BasemapToggle({
+//                 visibleElements: {
+//                     title: true,
+//                 },
+//                 view: view,
+//                 nextBasemap: 'satellite',
+//             });
+            
+//             // Add widget to the top right corner of the view
+//             view.ui.add(toggle, 'top-right');
+//         });
+     
+
+// // Create a new FeatureLayer instance with the specified URL
+// const featureLayer = new FeatureLayer({
+//     // url: 'https://services8.arcgis.com/T51xV83kzxEuohfL/arcgis/rest/services/water_fountains/FeatureServer'
+//     url: 'https://services8.arcgis.com/T51xV83kzxEuohfL/arcgis/rest/services/maui_fresh_water_fountain_locations/FeatureServer/0'
+// });
+
+// // Add the FeatureLayer to the map 
+// map.add(featureLayer);
+
+// // Optionally, set a renderer for the feature layer
+// const renderer = {
+//     type: 'simple',
+//     symbol: {
+//         type: 'simple-marker',
+//         url: "https://drive.google.com/uc?id=1RE4mTAvWbMKh9XQ9US5Nx_7TXgDjZUkt",
+//         color: '#06b6d4',
+//         size: 8,
+//         outline: {
+//             color: [0, 0, 0, 0], // Specify transparent outline
+//             width: 0 // Set outline width to 0 to make it invisible
+//         }
+//     }
+// };
+// featureLayer.renderer = renderer;
+
+// // Optionally, set a popup template for the feature layer
+// const popupTemplate = {
+//     title: '{Location}',
+//     content: [
+//         {
+//             type: 'text',
+//             text: 'Drinking Fountain'
+//         },
+//         {
+//             type: 'text',
+//             text: '<a href="https://www.google.com/maps/search/?api=1&query={Latitude},{Longitude}" target="_blank">Get Directions</a>'
+//         },
+//         {
+//             type: 'text',
+//             text: `<a href="https://www.mauinuiahupuaaproject.com/ahupuaa" target="_blank">Learn More About Maui's Water</a>`
+//         },
+//         {
+//             type: 'media',
+//             mediaInfos: [
+//                 {
+//                     type: 'image',
+//                     value: {
+//                         sourceURL: '{image}',
+//                         width: '25vw', // Adjust the width of the image to fit the popup
+//                         height: 'auto' // Maintain aspect ratio
+//                     }
+//                 }
+//             ]
+//         },
+
+//         {
+//             type: 'text',
+//             text: '<a href="https://www.google.com/maps/search/?api=1&query={Latitude},{Longitude}" target="_blank">Get Directions</a>'
+//         }
+//     ]
+// };
+// featureLayer.popupTemplate = popupTemplate;
+
+
+//     };
+        
+//       initializeArcGIS();
+//     }, []);
+
+//     return <div id="viewDiv" style={{ height: '100vh', width: '100vw', padding: 0, margin: 0 }}></div>;
+//   };
+  
+//   export default HomePage;
+
 import React, { useState, useEffect } from 'react';
 import esriConfig from '@arcgis/core/config';
 import Map from '@arcgis/core/Map';
-// import axios from 'axios';
 import VectorTileLayer from '@arcgis/core/layers/VectorTileLayer';
 import Basemap from '@arcgis/core/Basemap';
 import BasemapToggle from '@arcgis/core/widgets/BasemapToggle';
 import SceneView from '@arcgis/core/views/SceneView';
 import FeatureLayer from '@arcgis/core/layers/FeatureLayer';
-// import Symbol from "@arcgis/core/symbols/Symbol.js";
-// import Renderer from '@arcgis/core/renderers/Renderer.js';
-import PopupTemplate from "@arcgis/core/PopupTemplate.js";
-// import WebStyleSymbol from "@arcgis/core/symbols/WebStyleSymbol.js";
-// import SimpleRenderer from "@arcgis/core/renderers/SimpleRenderer.js";
-// import calciteIcons from "@esri/calcite-ui-icons/docs/icons.json";
-// import { loadModules } from 'esri-loader';
-import { TailSpin } from "react-loader-spinner";
-
+import PopupTemplate from '@arcgis/core/PopupTemplate';
+import { TailSpin } from 'react-loader-spinner';
+import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css';
 
 const HomePage = () => {
-    useEffect(() => {
-      const initializeArcGIS = async () => {
-        esriConfig.apiKey = process.env.REACT_APP_ESRI_KEY; // Set your API key from environment variables
-        // esriConfig.apiKey = process.env.REACT_APP_MAP_FEATUREDLAYER_KEY; // Set your API key from environment variables
-  
-        // Loading spinner
-            const [loading, setLoading] = useState(false);
-          
-            useEffect(() => {
-              setLoading(true);
-              setTimeout(() => {
-                setLoading(false);
-              }, 3000);
-            }, []);
+  const [loading, setLoading] = useState(true);
 
+  useEffect(() => {
+    const initializeArcGIS = async () => {
+      // Set your API key from environment variables
+      esriConfig.apiKey = process.env.REACT_APP_ESRI_KEY;
 
+      // Simulate API loading delay for demonstration
+      setTimeout(() => {
+        setLoading(false);
+      }, 3000);
+
+      // Initialize ArcGIS components after API key is set
+      if (!loading) {
         // Create a VectorTileLayer from a style URL
         const mapBaseLayer = new VectorTileLayer({
-          url: 'https://arcgis.com/sharing/rest/content/items/b5676525747f499687f12746441101ef/resources/styles/root.json',
+          url:
+            'https://arcgis.com/sharing/rest/content/items/b5676525747f499687f12746441101ef/resources/styles/root.json',
         });
-  
+
         // Create a Basemap with the VectorTileLayer
         const customBasemap = new Basemap({
-            baseLayers: [mapBaseLayer],
-            title: 'Street',
-            id: 'streets',
-            thumbnailUrl:
+          baseLayers: [mapBaseLayer],
+          title: 'Street',
+          id: 'streets',
+          thumbnailUrl:
             'https://arcgis.com/sharing/rest/content/items/b5676525747f499687f12746441101ef/info/thumbnail/ago_downloaded.png',
         });
-        
+
         const map = new Map({
-            basemap: 'arcgis/topographic', //streets-navigation-vector, arcgis/topographic, customBasemap
-            ground: 'world-elevation',
+          basemap: customBasemap,
+          ground: 'world-elevation',
         });
 
         const initCamera = {
-            heading: 1,
-            tilt: 1,
-            position: {
-                latitude: 20.753,
-                longitude: -156.449,
-                z: 200000,
-            },
+          heading: 1,
+          tilt: 1,
+          position: {
+            latitude: 20.753,
+            longitude: -156.449,
+            z: 200000,
+          },
         };
-        
+
         const view = new SceneView({
-            container: 'viewDiv',
-            map: map,
-            camera: initCamera,
+          container: 'viewDiv',
+          map: map,
+          camera: initCamera,
         });
-        
+
         view.when(() => {
-            // Add a BasemapToggle widget to toggle between basemaps
-            const toggle = new BasemapToggle({
-                visibleElements: {
-                    title: true,
-                },
-                view: view,
-                nextBasemap: 'satellite',
-            });
-            
-            // Add widget to the top right corner of the view
-            view.ui.add(toggle, 'top-right');
+          // Add a BasemapToggle widget to toggle between basemaps
+          const toggle = new BasemapToggle({
+            visibleElements: {
+              title: true,
+            },
+            view: view,
+            nextBasemap: 'satellite',
+          });
+
+          // Add widget to the top right corner of the view
+          view.ui.add(toggle, 'top-right');
         });
-     
 
-// Create a new FeatureLayer instance with the specified URL
-const featureLayer = new FeatureLayer({
-    // url: 'https://services8.arcgis.com/T51xV83kzxEuohfL/arcgis/rest/services/water_fountains/FeatureServer'
-    url: 'https://services8.arcgis.com/T51xV83kzxEuohfL/arcgis/rest/services/maui_fresh_water_fountain_locations/FeatureServer'
-});
+        // Create a new FeatureLayer instance with the specified URL
+        const featureLayer = new FeatureLayer({
+          url:
+            'https://services8.arcgis.com/T51xV83kzxEuohfL/arcgis/rest/services/maui_fresh_water_fountain_locations/FeatureServer',
+        });
 
-// Add the FeatureLayer to the map 
-map.add(featureLayer);
+        // Add the FeatureLayer to the map
+        map.add(featureLayer);
 
-// Optionally, set a renderer for the feature layer
-const renderer = {
-    type: 'simple',
-    symbol: {
-        type: 'simple-marker',
-        url: "https://drive.google.com/uc?id=1RE4mTAvWbMKh9XQ9US5Nx_7TXgDjZUkt",
-        color: '#06b6d4',
-        size: 8,
-        outline: {
-            color: [0, 0, 0, 0], // Specify transparent outline
-            width: 0 // Set outline width to 0 to make it invisible
-        }
-    }
-};
-featureLayer.renderer = renderer;
+        // Optionally, set a renderer for the feature layer
+        const renderer = {
+          type: 'simple',
+          symbol: {
+            type: 'simple-marker',
+            url:
+              'https://drive.google.com/uc?id=1RE4mTAvWbMKh9XQ9US5Nx_7TXgDjZUkt',
+            color: '#06b6d4',
+            size: 8,
+            outline: {
+              color: [0, 0, 0, 0], // Specify transparent outline
+              width: 0, // Set outline width to 0 to make it invisible
+            },
+          },
+        };
+        featureLayer.renderer = renderer;
 
-// Optionally, set a popup template for the feature layer
-const popupTemplate = {
-    title: '{Location}',
-    content: [
-        {
-            type: 'text',
-            text: 'Drinking Fountain'
-        },
-        {
-            type: 'text',
-            text: '<a href="https://www.google.com/maps/search/?api=1&query={Latitude},{Longitude}" target="_blank">Get Directions</a>'
-        },
-        {
-            type: 'text',
-            text: `<a href="https://www.mauinuiahupuaaproject.com/ahupuaa" target="_blank">Learn More About Maui's Water</a>`
-        },
-        {
-            type: 'media',
-            mediaInfos: [
+        // Optionally, set a popup template for the feature layer
+        const popupTemplate = new PopupTemplate({
+          title: '{Location}',
+          content: [
+            {
+              type: 'text',
+              text: 'Drinking Fountain',
+            },
+            {
+              type: 'text',
+              text: `<a href="https://www.google.com/maps/search/?api=1&query={Latitude},{Longitude}" target="_blank">Get Directions</a>`,
+            },
+            {
+              type: 'text',
+              text: `<a href="https://www.mauinuiahupuaaproject.com/ahupuaa" target="_blank">Learn More About Maui's Water</a>`,
+            },
+            {
+              type: 'media',
+              mediaInfos: [
                 {
-                    type: 'image',
-                    value: {
-                        sourceURL: '{image}',
-                        width: '25vw', // Adjust the width of the image to fit the popup
-                        height: 'auto' // Maintain aspect ratio
-                    }
-                }
-            ]
-        },
-
-        {
-            type: 'text',
-            text: '<a href="https://www.google.com/maps/search/?api=1&query={Latitude},{Longitude}" target="_blank">Get Directions</a>'
-        }
-    ]
-};
-featureLayer.popupTemplate = popupTemplate;
-
-
+                  type: 'image',
+                  value: {
+                    sourceURL: '{image}',
+                    width: '25vw', // Adjust the width of the image to fit the popup
+                    height: 'auto', // Maintain aspect ratio
+                  },
+                },
+              ],
+            },
+          ],
+        });
+        featureLayer.popupTemplate = popupTemplate;
+      }
     };
-        
-      initializeArcGIS();
-    }, []);
 
-    return (
+    initializeArcGIS();
+  }, []);
+
+  return (
     <>
-        <div>{loading && <TailSpin />}</div>
-        <div id="viewDiv" style={{ height: '100vh', width: '100vw', padding: 0, margin: 0 }}></div>;
+      <div style={{ position: 'relative', minHeight: '100vh' }}>
+        {loading && (
+          <div
+            style={{
+              position: 'absolute',
+              top: '50%',
+              left: '50%',
+              transform: 'translate(-50%, -50%)',
+              zIndex: 9999,
+            }}
+          >
+            <TailSpin color="#06b6d4" height={50} width={50} />
+            <p>Loading Map...</p>
+          </div>
+        )}
+        <div id="viewDiv" style={{ height: '100vh', width: '100vw', padding: 0, margin: 0 }}></div>
+      </div>
     </>
-    );
-  };
-  
-  export default HomePage;
+  );
+};
+
+export default HomePage;
