0519ff62bb0428caab7a864c7d0ff71527f462d5
robin hunter <rbnrggs@yahoo.com>
2024-02-27 15:42:28 -1000

    error fix

--- frontend/src/components/LearnMorePage/index.jsx
+++ frontend/src/components/LearnMorePage/index.jsx
@@ -1,19 +1,3 @@
-// export default function LearnMorePage() {   
-
-//     return (
-//         <>
-//         <div className="bg-neutral-200 h-screen w-screen">
-//             <br />
-//             <h1 className='pl-4 text-lg text-center'>Add content here</h1>
-//             <br />
-//             <h1 className='pl-4 text-lg text-center'></h1>
-//             <br />
-//             {/* <p className="mx-auto font-light w-[75vw]">More Resources | County of Maui, Department of Water<a href="https://www.mauicounty.gov/QuickLinks.aspx?CID=43#:~:text=watersheds%20are%20drinking%20water%20source,in%20West%20and%20Central%20Maui."><span className="text-pink-300 hover:text-teal-200">Mauicounty.gov/QuickLinks</span></a></p> */}
-//         </div>
-//         </>
-//     )
-// }
-
 import React, { useEffect } from 'react';
 import mapboxgl from 'mapbox-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,14 +3,16 @@ import ReactDOM from 'react-dom'
 import { BrowserRouter as Router } from "react-router-dom"
 import App from './components/App'
 import './index.css'
-
 import { ThemeProvider } from '@material-tailwind/react'
- 
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+// Set Mapbox access token
+mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_ACCESS_TOKEN;
+
+ReactDOM.render(
   <Router>
     <ThemeProvider>
-    <App />
+      <App />
     </ThemeProvider>
   </Router>,
-)
+  document.getElementById('root')
+);
