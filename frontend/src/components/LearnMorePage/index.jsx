d07a8e521ae782dee353792233d3f7e77790102f
robin hunter <rbnrggs@yahoo.com>
2024-03-01 13:38:16 -1000

    correct error with map load

--- frontend/src/components/LearnMorePage/index.jsx
+++ frontend/src/components/LearnMorePage/index.jsx
@@ -1,86 +1,106 @@
-// export default function LearnMorePage() {   
+// import React from 'react';
+// import React, { useRef, useEffect, useState } from 'react';
+// import mapboxgl from 'mapbox-gl'; // Import Mapbox GL JS
+// import mapboxgl from '!mapbox-gl';
+// import ReactDOM from 'react-dom';
+// import 'mapbox-gl/dist/mapbox-gl.css';
+// import './index.css';
 
-//   return (
-//       <>
-//       <div className="bg-neutral-200 h-screen w-screen">
-//           <br />
-//           <h1 className='pl-4 text-lg text-center'>Add content here</h1>
-//           <br />
-//           <h1 className='pl-4 text-lg text-center'></h1>
-//           <br />
-//           {/* <p className="mx-auto font-light w-[75vw]">More Resources | County of Maui, Department of Water<a href="https://www.mauicounty.gov/QuickLinks.aspx?CID=43#:~:text=watersheds%20are%20drinking%20water%20source,in%20West%20and%20Central%20Maui."><span className="text-pink-300 hover:text-teal-200">Mauicounty.gov/QuickLinks</span></a></p> */}
-//       </div>
-//       </>
-//   )
-// }
-
-// import React, { useEffect } from "react";
-// import mapboxgl from "mapbox-gl";
-// import "./mapboxGl.css";
-// import mauiLocations from "./mauiLocations.geojson";
-
+// import mauiLocations from './mauiLocations.geojson'; // Import data for Maui locations
 
 // const LearnMorePage = () => {
+// const mapContainer = useRef(null);
+// const map = useRef(null);
+// const [lng, setLng] = useState(-156.700589);
+// const [lat, setLat] = useState(20.816663);
+// const [zoom, setZoom] = useState(9);
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//   <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+//   );
+
+//   // useEffect(() => {
+//   //   // Set Mapbox access token from environment variable
+//   //   mapboxgl.accessToken = import.meta.env.VITE_MAPBOX_ACCESS_TOKEN;
+
+//   //   // Initialize a new map
+//   //   const map = new mapboxgl.Map({
+//   //     container: 'map',
+//   //     style: 'mapbox://styles/mapbox/streets-v9',
+//   //     center: [-156.700589, 20.816663], // Initial map center (longitude, latitude)
+//   //     zoom: 9, // Initial zoom level
+//   //     pitchWithRotate: false,
+//   //   });
+
 //   useEffect(() => {
-
-//     mapboxgl.accessToken = import.meta.env.VITE_MAPBOX_ACCESS_TOKEN; // this variable in your .env file
-
-//     const map = new mapboxgl.Map({
-//       container: "map",
-//       style: "mapbox://styles/mapbox/streets-v9",
-//       center: [-156.700589, 20.816663], // lng, lat
-//       zoom: 9,
-//       pitchWithRotate: false,
+//     if (map.current) return; // initialize map only once
+//     map.current = new mapboxgl.Map({
+//       container: mapContainer.current,
+//       style: 'mapbox://styles/mapbox/streets-v12',
+//       center: [lng, lat],
+//       zoom: zoom
 //     });
 
-//     map.addControl(new mapboxgl.NavigationControl(), "top-left");
+//     // Add navigation control to the map
+//     map.addControl(new mapboxgl.NavigationControl(), 'top-left');
 
-//     map.on("load", () => {
-//       map.setFog({});
-//       // Load and add the image for custom markers
+//     // Wait for the map to load
+//     map.on('style.load', () => {
+//       map.setFog({}); // Set fog effect if needed
+
+//       // Add a GeoJSON source and a layer for Maui locations
+//       map.addSource('mauiLocations', {
+//         type: 'geojson',
+//         data: './mauiLocations.geojson', // GeoJSON data for Maui locations
+//       });
+
+//       // Load custom marker image
 //       map.loadImage(
-//         "https://raw.githubusercontent.com/robinahunter/maui-fountain-images/main/water-drop-shadow-maui.png",
+//         'https://raw.githubusercontent.com/robinahunter/maui-fountain-images/main/water-drop-shadow-maui.png',
 //         (error, image) => {
 //           if (error) throw error;
 
-//           map.addImage("custom-marker", image);
+//           // Add custom marker image to the map
+//           map.addImage('custom-marker', image);
 
-//           map.addSource("mauiLocations", {
-//             type: "geojson",
-//             data: mauiLocations,
-//           });
-
+//           // Add a layer for displaying custom markers
 //           map.addLayer({
-//             id: "mauiLocations-symbol",
-//             type: "symbol",
-//             source: "mauiLocations",
+//             id: 'mauiLocations-symbol',
+//             type: 'symbol',
+//             source: 'mauiLocations',
 //             layout: {
-//               "icon-image": "custom-marker",
-//               "icon-allow-overlap": true,
-//               "icon-size": 0.09,
+//               'icon-image': 'custom-marker',
+//               'icon-allow-overlap': true,
+//               'icon-size': 0.09,
 //             },
 //           });
 
-//           map.on("click", "mauiLocations-symbol", (e) => {
+//           // Add a click event listener to the markers
+//           map.on('click', 'mauiLocations-symbol', (e) => {
 //             const coordinates = e.features[0].geometry.coordinates.slice();
 //             const properties = e.features[0].properties;
 
+//             // Construct HTML content for the popup
 //             let popupContent = `<h3>${properties.Location}</h3>`;
 
 //             if (properties.Image) {
 //               popupContent += `
-//                                 <div style="max-width: 200px; max-height: 150px; overflow: hidden;">
-//                                     <img src="${properties.Image}" alt="${properties.Location}" style="width: 100%; height: 100%; object-fit: cover;" />
-//                                 </div>`;
+//                 <div style="max-width: 200px; max-height: 150px; overflow: hidden;">
+//                   <img src="${properties.Image}" alt="${properties.Location}" style="width: 100%; height: 100%; object-fit: cover;" />
+//                 </div>`;
 //             }
 
 //             popupContent += `
-//                             <p><a href="https://www.google.com/maps/search/?api=1&query=${coordinates[1]},${coordinates[0]}" target="_blank">Get Directions</a></p>
-//                             <p>${properties.Type || ""}</p>
-//                             <p>${properties.Address || ""}, ${properties.City || ""}, ${properties.State || ""}</p>
-//                             <p>${parseFloat(coordinates[1]).toFixed(2)}, ${parseFloat(coordinates[0]).toFixed(2)}</p>
-//                             <a href="https://www.mauinuiahupuaaproject.com/ahupuaa" target="_blank">Learn More About Maui's Water</a>`;
+//               <p><a href="https://www.google.com/maps/search/?api=1&query=${coordinates[1]},${coordinates[0]}" target="_blank">Get Directions</a></p>
+//               <p>${properties.Type || ''}</p>
+//               <p>${properties.Address || ''}, ${properties.City || ''}, ${properties.State || ''}</p>
+//               <p>${parseFloat(coordinates[1]).toFixed(2)}, ${parseFloat(coordinates[0]).toFixed(2)}</p>
+//               <a href="https://www.mauinuiahupuaaproject.com/ahupuaa" target="_blank">Learn More About Maui's Water</a>`;
 
+//             // Create and display a popup on the map
 //             new mapboxgl.Popup()
 //               .setLngLat(coordinates)
 //               .setHTML(popupContent)
@@ -88,198 +108,110 @@
 //           });
 //         },
 //       );
-
-//       // Clean up function
-//       return () => map.remove();
 //     });
-//   }, []);
 
-//   return <div id="map" style={{ height: "100%", width: "100%", padding: 0, margin: 0 }} />;
-// };
+//     return () => map.remove(); // Cleanup function to remove the map
+//   }, []); // Empty dependency array ensures this effect runs only once
+
+//   return (
+//     <div>
+//     <div ref={mapContainer} className="map-container" />
+//     </div>
+//     );
 
 // export default LearnMorePage;
 
 
-
-// import React, { useEffect } from "react";
-// import mapboxgl from "mapbox-gl";
-// import "mapbox-gl/dist/mapbox-gl.css";
-// import mauiLocations from "./mauiLocations.geojson";
-
-// const LearnMorePage = () => {
-//   useEffect(() => {
-//     mapboxgl.accessToken = import.meta.env.VITE_MAPBOX_ACCESS_TOKEN; // this variable in your .env file
-
-//     const map = new mapboxgl.Map({
-//       container: "map",
-//       style: "mapbox://styles/mapbox/streets-v9",
-//       center: [-156.700589, 20.816663], // lng, lat
-//       zoom: 9,
-//       pitchWithRotate: false,
-//     });
-
-//     map.addControl(new mapboxgl.NavigationControl(), "top-left");
-
-//     map.on("load", () => {
-//       map.setFog({});
-//       // Load and add the image for custom markers
-//       map.loadImage(
-//         "https://raw.githubusercontent.com/robinahunter/maui-fountain-images/main/water-drop-shadow-maui.png",
-//         (error, image) => {
-//           if (error) throw error;
-
-//           map.addImage("custom-marker", image);
-
-//           map.addSource("mauiLocations", {
-//             type: "geojson",
-//             data: mauiLocations, 
-//           });
-
-//           map.addLayer({
-//             id: "mauiLocations-symbol",
-//             type: "symbol",
-//             source: "mauiLocations",
-//             layout: {
-//               "icon-image": "custom-marker",
-//               "icon-allow-overlap": true,
-//               "icon-size": 0.09,
-//             },
-//           });
-
-//           map.on("click", "mauiLocations-symbol", (e) => {
-//             if (!e.features || !e.features.length) return;
-        
-//             const coordinates = e.features[0].geometry.coordinates.slice();
-//             const properties = e.features[0].properties;
-        
-//             let popupContent = `<h3>${properties.Location}</h3>`;
-        
-//             if (properties.Image) {
-//                 popupContent += `
-//                     <div style="max-width: 200px; max-height: 150px; overflow: hidden;">
-//                         <img src="${properties.Image}" alt="${properties.Location}" style="width: 100%; height: 100%; object-fit: cover;" />
-//                     </div>`;
-//             }
-        
-//             popupContent += `
-//                 <p><a href="https://www.google.com/maps/search/?api=1&query=${coordinates[1]},${coordinates[0]}" target="_blank">Get Directions</a></p>
-//                 <p>${properties.Type || ""}</p>
-//                 <p>${properties.Address || ""}, ${properties.City || ""}, ${properties.State || ""}</p>
-//                 <p>${parseFloat(coordinates[1]).toFixed(2)}, ${parseFloat(coordinates[0]).toFixed(2)}</p>
-//                 <a href="https://www.mauinuiahupuaaproject.com/ahupuaa" target="_blank">Learn More About Maui's Water</a>`;
-        
-//             new mapboxgl.Popup()
-//                 .setLngLat(coordinates)
-//                 .setHTML(popupContent)
-//                 .addTo(map);
-//         });
-//         },
-//       );
-
-//       // Clean up function
-//       return () => map.remove();
-//     });
-//   }, []);
-
-//   return <div id="map" style={{ height: "100%", width: "100%", padding: 0, margin: 0 }} />;
-// };
-
-// export default LearnMorePage;
-
-import { useEffect } from 'react';
+import React, { useEffect, useRef } from 'react';
 import mapboxgl from 'mapbox-gl'; // Import Mapbox GL JS
+import ReactDOM from 'react-dom';
+import 'mapbox-gl/dist/mapbox-gl.css';
 import mauiLocations from './mauiLocations.geojson'; // Import data for Maui locations
-import './mapboxGl.css';
 
 const LearnMorePage = () => {
+  const mapContainer = useRef(null);
+  const map = useRef(null);
+  const lng = -156.700589;
+  const lat = 20.816663;
+  const zoom = 9;
+
   useEffect(() => {
-    // Set Mapbox access token from environment variable
-    mapboxgl.accessToken = import.meta.env.VITE_MAPBOX_ACCESS_TOKEN;
+    mapboxgl.accessToken = 'YOUR_MAPBOX_ACCESS_TOKEN';
 
-    // Initialize a new map
-    const map = new mapboxgl.Map({
-      container: 'map',
-      style: 'mapbox://styles/mapbox/streets-v9',
-      center: [-156.700589, 20.816663], // Initial map center (longitude, latitude)
-      zoom: 9, // Initial zoom level
-      pitchWithRotate: false,
-    });
+    if (!map.current) {
+      map.current = new mapboxgl.Map({
+        container: mapContainer.current,
+        style: 'mapbox://styles/mapbox/streets-v12',
+        center: [lng, lat],
+        zoom: zoom
+      });
 
-    // Add navigation control to the map
-    map.addControl(new mapboxgl.NavigationControl(), 'top-left');
+      map.current.addControl(new mapboxgl.NavigationControl(), 'top-left');
 
-    // Wait for the map to load
-    map.on('load', () => {
-      map.setFog({}); // Set fog effect if needed
+      map.current.on('load', () => {
+        map.current.setFog({}); // Set fog effect if needed
 
-      // Load custom marker image
-      map.loadImage(
-        'https://raw.githubusercontent.com/robinahunter/maui-fountain-images/main/water-drop-shadow-maui.png',
-        (error, image) => {
-          if (error) throw error;
+        map.current.addSource('mauiLocations', {
+          type: 'geojson',
+          data: mauiLocations // GeoJSON data for Maui locations
+        });
 
-          // Add custom marker image to the map
-          map.addImage('custom-marker', image);
+        map.current.loadImage(
+          'https://raw.githubusercontent.com/robinahunter/maui-fountain-images/main/water-drop-shadow-maui.png',
+          (error, image) => {
+            if (error) throw error;
 
-          // Add a GeoJSON source and a layer for Maui locations
-          map.addSource('mauiLocations', {
-            type: 'geojson',
-            data: mauiLocations, // GeoJSON data for Maui locations
-          });
+            map.current.addImage('custom-marker', image);
 
-          // Add a layer for displaying custom markers
-          map.addLayer({
-            id: 'mauiLocations-symbol',
-            type: 'symbol',
-            source: 'mauiLocations',
-            layout: {
-              'icon-image': 'custom-marker',
-              'icon-allow-overlap': true,
-              'icon-size': 0.09,
-            },
-          });
+            map.current.addLayer({
+              id: 'mauiLocations-symbol',
+              type: 'symbol',
+              source: 'mauiLocations',
+              layout: {
+                'icon-image': 'custom-marker',
+                'icon-allow-overlap': true,
+                'icon-size': 0.09
+              }
+            });
 
-          // Add a click event listener to the markers
-          map.on('click', 'mauiLocations-symbol', (e) => {
-            if (!e.features || !e.features.length) return;
+            map.current.on('click', 'mauiLocations-symbol', (e) => {
+              const coordinates = e.features[0].geometry.coordinates.slice();
+              const properties = e.features[0].properties;
 
-            // Get coordinates and properties of the clicked feature
-            const coordinates = e.features[0].geometry.coordinates.slice();
-            const properties = e.features[0].properties;
+              let popupContent = `<h3>${properties.Location}</h3>`;
 
-            // Construct HTML content for the popup
-            let popupContent = `<h3>${properties.Location}</h3>`;
+              if (properties.Image) {
+                popupContent += `
+                  <div style="max-width: 200px; max-height: 150px; overflow: hidden;">
+                    <img src="${properties.Image}" alt="${properties.Location}" style="width: 100%; height: 100%; object-fit: cover;" />
+                  </div>`;
+              }
 
-            if (properties.Image) {
               popupContent += `
-                <div style="max-width: 200px; max-height: 150px; overflow: hidden;">
-                  <img src="${properties.Image}" alt="${properties.Location}" style="width: 100%; height: 100%; object-fit: cover;" />
-                </div>`;
-            }
+                <p><a href="https://www.google.com/maps/search/?api=1&query=${coordinates[1]},${coordinates[0]}" target="_blank">Get Directions</a></p>
+                <p>${properties.Type || ''}</p>
+                <p>${properties.Address || ''}, ${properties.City || ''}, ${properties.State || ''}</p>
+                <p>${parseFloat(coordinates[1]).toFixed(2)}, ${parseFloat(coordinates[0]).toFixed(2)}</p>
+                <a href="https://www.mauinuiahupuaaproject.com/ahupuaa" target="_blank">Learn More About Maui's Water</a>`;
 
-            popupContent += `
-              <p><a href="https://www.google.com/maps/search/?api=1&query=${coordinates[1]},${coordinates[0]}" target="_blank">Get Directions</a></p>
-              <p>${properties.Type || ''}</p>
-              <p>${properties.Address || ''}, ${properties.City || ''}, ${properties.State || ''}</p>
-              <p>${parseFloat(coordinates[1]).toFixed(2)}, ${parseFloat(coordinates[0]).toFixed(2)}</p>
-              <a href="https://www.mauinuiahupuaaproject.com/ahupuaa" target="_blank">Learn More About Maui's Water</a>`;
+              new mapboxgl.Popup()
+                .setLngLat(coordinates)
+                .setHTML(popupContent)
+                .addTo(map.current);
+            });
+          }
+        );
+      });
 
-            // Create and display a popup on the map
-            new mapboxgl.Popup()
-              .setLngLat(coordinates)
-              .setHTML(popupContent)
-              .addTo(map);
-          });
-        },
-      );
+      return () => map.current.remove(); // Cleanup function to remove the map
+    }
+  }, []); // Empty dependency array ensures this effect runs only once
 
-      // Clean up function to remove the map
-      return () => map.remove();
-    });
-  }, []); // Run useEffect only once on component mount
-
-  // Render the map container
-  return <div id="map" style={{ height: '100%', width: '100%', padding: 0, margin: 0 }} />;
+  return (
+    <div>
+      <div ref={mapContainer} className="map-container" />
+    </div>
+  );
 };
 
 export default LearnMorePage;
